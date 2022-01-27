0c92fbedcd56f524bc931013d713ef2e1bbeb691
Quentin Freire Novo <yushi76240@gmail.com>
2022-01-27 02:43:49 -0500

    Premier commit

---
+++ dev/FasterThanPixels/FasterThanPixels.cpp
@@ -0,0 +1,30 @@
+// FasterThanPixels.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "Box2D/Box2D.h"
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
