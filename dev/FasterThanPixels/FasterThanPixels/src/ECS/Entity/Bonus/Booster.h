6f20a224f053a12e84948268c135adca6a79dfa6
BoyerFlorian <boyerflorian95@gmail.com>
2022-03-02 21:46:29 -0500

    UML
    Synthèse de l'UML
    Suppression de l'entité Booster

--- dev/FasterThanPixels/FasterThanPixels/src/ECS/Entity/Bonus/Booster.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Engine.h"
-/*! \class Booster
-* \brief classe representant l'entite booster, elle apparait au sol et attend d'etre ramasser 
-*
-*/
-class Booster : public Engine::Entity {
-
-};
